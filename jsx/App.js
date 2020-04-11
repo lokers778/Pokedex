704e26dab580b7b597b900e561934dae2496e54d
Łukasz <lokers778@gmail.com>
2020-04-11 20:13:09 +0200

    fetch data/ first components concept / metatags

--- jsx/App.js
+++ jsx/App.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
 
-    render() {
-        return <div> hello pokedex </div>
+    componentDidMount() {
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=100&offset=100")
+            .then(res => res.json())
+            .then(res => console.log(res))
+    }
 
+    render() {
+        return <div className="container">
+            <div>replacement header</div>
+            <div>replacement pokemon list</div>
+            <div>replacement footer</div>
+        </div>
     }
 }
 
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
